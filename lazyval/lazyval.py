8d2b6b20c2c30123c5491183135c0bf026029c19
Eric Casteleijn <ec@trapit.com>
2013-08-06 23:14:15 -0700

    working schemas

--- lazyval/lazyval.py
+++ lazyval/lazyval.py
@@ -20,21 +20,7 @@ class Or(object):
         return " or ".join(["<%s>" % (v,) for v in self.values])
 
 
-def parse_schema(schema):
-    return schema
-
-
-class Schema(object):
-
-    def __init__(self, schema):
-        self.schema = parse_schema(schema)
-
-
-def _dict_item_validate(schema_mandatory, schema_optional, schema_types, key,
-                        value):
-    if key in schema_mandatory:
-        return validate(schema_mandatory[key], value)
-
+def _dict_item_validate(schema_optional, schema_types, key, value):
     if key in schema_optional:
         return validate(schema_optional[key], value)
 
@@ -60,6 +46,18 @@ def _list_item_validate(schema, value):
         except NotValid:
             pass
     raise NotValid('%r not validated by anything in %s.' % (value, schema))
+
+
+def _schema_list_item_validate(sub_schemas, value):
+    for sub in sub_schemas:
+        try:
+            return sub(value)
+
+        except NotValid:
+            pass
+
+    raise NotValid('%r not validated by anything in %s.' % (
+        value, sub_schemas))
 
 
 def validates(schema, data):
@@ -91,38 +89,152 @@ def validate(schema, data):
         if not isinstance(data, dict):
             raise NotValid('%r is not of type dict', (data,))
         validated = {}
-        not_seen = [
-            key for key in schema.keys() if not type(key) is type and not
-            isinstance(key, Optional)]
-        schema_optional = {}
-        schema_mandatory = {}
-        schema_types = {}
+        optional = {}
+        mandatory = {}
+        types = {}
         for key, value in schema.items():
             if isinstance(key, Optional):
-                schema_optional[key.value] = value
+                optional[key.value] = value
                 continue
 
             if type(key) is type:
-                schema_types[key] = value
+                types[key] = value
                 continue
 
-            schema_mandatory[key] = value
-        for key, val in data.items():
-            validated[key] = _dict_item_validate(
-                schema_mandatory, schema_optional, schema_types, key, val)
-            if key in not_seen:
-                not_seen.remove(key)
-        for key in not_seen:
-            if not isinstance(key, Optional):
+            mandatory[key] = value
+        to_validate = data.keys()
+        for key, sub_schema in mandatory.items():
+            if key not in data:
                 raise NotValid('missing key: %s' % (key,))
+            validated[key] = validate(sub_schema, data[key])
+            to_validate.remove(key)
+        for key in to_validate:
+            validated[key] = _dict_item_validate(
+                optional, types, key, data[key])
 
         return validated
 
     if type(data) is list:
+        if not isinstance(data, list):
+            raise NotValid('%r is not of type list', (data,))
+
         return [_list_item_validate(schema, i) for i in data]
 
-    if type(data) is type(schema):
+    if data == schema:
+        return data
+
+    raise NotValid('%r is not equal to %s' % (data, schema))
+
+
+def parse_schema(schema):
+    if isinstance(schema, Or):
+        sub_schemas = [parse_schema(v) for v in schema.values]
+
+        def or_validator(data):
+            for schema in sub_schemas:
+                try:
+                    return schema(data)
+                except NotValid:
+                    pass
+            raise NotValid('%r not validated by %s' % (data, schema))
+
+        return or_validator
+
+    if type(schema) is type:
+
+        def type_validator(data):
+            if isinstance(data, schema):
+                return data
+
+            raise NotValid('%r is not of type %s' % (data, schema))
+
+        return type_validator
+
+    if isinstance(schema, dict):
+
+        optional = {}
+        mandatory = {}
+        types = {}
+        for key, value in schema.items():
+            if isinstance(key, Optional):
+                optional[key.value] = parse_schema(value)
+                continue
+
+            if type(key) is type:
+                types[key] = parse_schema(value)
+                continue
+
+            mandatory[key] = parse_schema(value)
+
+        def dict_validator(data):
+            if not isinstance(data, dict):
+                raise NotValid('%r is not of type dict', (data,))
+            validated = {}
+            to_validate = data.keys()
+            for key, sub_schema in mandatory.items():
+                if key not in data:
+                    raise NotValid('missing key: %s' % (key,))
+                validated[key] = sub_schema(data[key])
+                to_validate.remove(key)
+            for key in to_validate[:]:
+                value = data[key]
+                if key in optional:
+                    validated[key] = optional[key](value)
+                    to_validate.remove(key)
+                    continue
+                for key_schema, value_schema in types.items():
+                    if not isinstance(key, key_schema):
+                        continue
+                    try:
+                        validated[key] = value_schema(value)
+                    except NotValid:
+                        continue
+                    else:
+                        to_validate.remove(key)
+                        break
+                else:
+                    raise NotValid('key %r not matched' % (key,))
+
+            if to_validate:
+                raise NotValid('keys %s not matched' % (to_validate,))
+            return validated
+
+        return dict_validator
+
+    if isinstance(schema, list):
+
+        sub_schemas = [parse_schema(s) for s in schema]
+
+        def list_validator(data):
+            if not isinstance(data, list):
+                raise NotValid('%r is not of type list', (data,))
+
+            return [
+                _schema_list_item_validate(sub_schemas, value)
+                for value in data]
+
+        return list_validator
+
+    def static_validator(data):
         if data == schema:
             return data
 
-    raise NotValid('%r is not equal to %s' % (data, schema))
+        raise NotValid('%r is not equal to %s' % (data, schema))
+
+    return static_validator
+
+
+class Schema(object):
+
+    def __init__(self, schema):
+        self.schema = parse_schema(schema)
+
+    def validate(self, data):
+        return self.schema(data)
+
+    def validates(self, data):
+        try:
+            self.schema(data)
+            return True
+        except NotValid:
+            return False
--- lazyval/test_lazyval.py
+++ lazyval/test_lazyval.py
@@ -1,7 +1,9 @@
-from lazyval import validate, validates, NotValid, Optional, Or
+from tempfile import mkstemp
+from lazyval import validate, validates, NotValid, Optional, Or, Schema
 from unittest import TestCase
+from hotshot import Profile, stats
 
-lazy_schema = {
+LAZY_SCHEMA = {
     Optional('invisible'): bool,
     Optional('immutable'): bool,
     Optional('favorite_colors'): [str],
@@ -19,8 +21,8 @@ lazy_schema = {
 TYPICAL_TEST_DATA = {
     'invisible': False,
     'immutable': False,
-    'favorite_colors': [],
-    'favorite_foods': [],
+    'favorite_colors': ['mauve', 'taupe', 'beige'],
+    'favorite_foods': ['granola', 'shinola'],
     'lucky_number': 1,
     'shoe_size': 12,
     'mother': {
@@ -33,8 +35,8 @@ TYPICAL_TEST_DATA = {
 INVALID_TEST_DATA = {
     'invisible': False,
     'immutable': 0,
-    'favorite_colors': [],
-    'favorite_foods': [],
+    'favorite_colors': ['test', 'test2', None, 1233, 1.34, False],
+    'favorite_foods': ['kraft cheese'],
     'lucky_number': 1,
     'shoe_size': 12,
     'mother': 'edna',
@@ -48,8 +50,16 @@ class TestLazyval(TestCase):
     def test_identity(self):
         self.assertEquals(validate('test', 'test'), 'test')
 
+    def test_identity_schema(self):
+        schema = Schema('test')
+        self.assertEquals(schema.validate('test'), 'test')
+
     def test_non_identity(self):
         self.assertRaises(NotValid, validate, 'foo', 'bar')
+
+    def test_non_identity_schema(self):
+        schema = Schema('test')
+        self.assertRaises(NotValid, schema.validate, 'bar')
 
     def test_validates_true(self):
         self.assertEquals(validates('foo', 'foo'), True)
@@ -60,42 +70,90 @@ class TestLazyval(TestCase):
     def test_type_check(self):
         self.assertEquals(validate(str, 'test'), 'test')
 
+    def test_type_check_schema(self):
+        schema = Schema(str)
+        self.assertEquals(schema.validate('test'), 'test')
+
     def test_failing_type_check(self):
         self.assertRaises(NotValid, validate, int, 'test')
+
+    def test_failing_type_check_schema(self):
+        schema = Schema(int)
+        self.assertRaises(NotValid, schema.validate, 'test')
 
     def test_dictionary(self):
         self.assertEquals(
             validate({'key': str}, {'key': 'val'}), {'key': 'val'})
+
+    def test_dictionary_schema(self):
+        schema = Schema({'key': str})
+        self.assertEquals(schema.validate({'key': 'val'}), {'key': 'val'})
 
     def test_dictionary_optional(self):
         self.assertEquals(
             validate({'key': str, Optional('key2'): str}, {'key': 'val'}),
             {'key': 'val'})
 
+    def test_dictionary_optional_schema(self):
+        schema = Schema({'key': str, Optional('key2'): str})
+        self.assertEquals(schema.validate({'key': 'val'}), {'key': 'val'})
+
     def test_dictionary_wrong_key(self):
         self.assertRaises(NotValid, validate, {'key': str}, {'not_key': 'val'})
+
+    def test_dictionary_wrong_key_schema(self):
+        schema = Schema({'key': str})
+        self.assertRaises(NotValid, schema.validate, {'not_key': 'val'})
 
     def test_dictionary_missing_key(self):
         self.assertRaises(
             NotValid, validate, {'key': str, 'key2': str}, {'key': 'val'})
 
+    def test_dictionary_missing_key_schema(self):
+        schema = Schema({'key': str, 'key2': str})
+        self.assertRaises(NotValid, schema.validate, {'key': 'val'})
+
     def test_list_data(self):
         self.assertTrue(validates([str], ['1', '2', '3']))
+
+    def test_list_data_schema(self):
+        schema = Schema([str])
+        self.assertTrue(schema.validates(['1', '2', '3']))
 
     def test_list_data_wrong_type(self):
         self.assertFalse(validates([str], ['1', '2', 3]))
 
+    def test_list_data_wrong_type_schema(self):
+        schema = Schema([str])
+        self.assertFalse(schema.validates(['1', '2', 3]))
+
     def test_list_data_multiple_types(self):
         self.assertTrue(validates([str, int], ['1', '2', 3]))
 
-    def test_list_schema(self):
+    def test_list_data_multiple_types_schema(self):
+        schema = Schema([str, int])
+        self.assertTrue(schema.validates(['1', '2', 3]))
+
+    def test_not_list(self):
         self.assertFalse(validates(['1', '2', '3'], '1'))
+
+    def test_not_list_schema(self):
+        schema = Schema(['1', '2', '3'])
+        self.assertFalse(schema.validates('1'))
 
     def test_or(self):
         self.assertTrue(validates(Or('1', '2', '3'), '1'))
 
-    def test_list_schema_not_found(self):
+    def test_or_schema(self):
+        schema = Schema(Or('1', '2', '3'))
+        self.assertTrue(schema.validates('1'))
+
+    def test_list_not_found(self):
         self.assertFalse(validates(['1', '2', '3'], '12'))
+
+    def test_list_not_found_schema(self):
+        schema = Schema(['1', '2', '3'])
+        self.assertFalse(schema.validates('12'))
 
     def test_dont_care_values_in_dict(self):
         self.assertEquals(
@@ -107,6 +165,22 @@ class TestLazyval(TestCase):
                  'bar': 'bar',
                  'qux': 'baz',
                  'fnord': [1, 2, 'donkey kong']}),
+            {'foo': 12,
+             'bar': 'bar',
+             'qux': 'baz',
+             'fnord': [1, 2, 'donkey kong']})
+
+    def test_dont_care_values_in_dict_schema(self):
+        schema = Schema(
+            {'foo': int,
+             'bar': str,
+             str: object})
+        self.assertEquals(
+            schema.validate(
+                {'foo': 12,
+                    'bar': 'bar',
+                    'qux': 'baz',
+                    'fnord': [1, 2, 'donkey kong']}),
             {'foo': 12,
              'bar': 'bar',
              'qux': 'baz',
@@ -125,7 +199,30 @@ class TestLazyval(TestCase):
 
     def test_schema_valid(self):
         self.assertEquals(
-            validate(lazy_schema, TYPICAL_TEST_DATA), TYPICAL_TEST_DATA)
+            validate(LAZY_SCHEMA, TYPICAL_TEST_DATA), TYPICAL_TEST_DATA)
 
     def test_schema_invalid(self):
-        self.assertRaises(NotValid, validate, lazy_schema, INVALID_TEST_DATA)
+        self.assertRaises(NotValid, validate, LAZY_SCHEMA, INVALID_TEST_DATA)
+
+    def test_functional_validation_profile(self):
+        tmp_file, filename = mkstemp()
+        profile = Profile(filename)
+        result = profile.runcall(validate, LAZY_SCHEMA, TYPICAL_TEST_DATA)
+        st = stats.load(filename)
+        st.strip_dirs()
+        st.sort_stats('time', 'calls')
+        st.print_stats(20)
+        self.assertTrue(result)
+        self.fail()
+
+    def test_schema_parsing_profile(self):
+        tmp_file, filename = mkstemp()
+        profile = Profile(filename)
+        lazy_schema = Schema(LAZY_SCHEMA)
+        result = profile.runcall(lazy_schema.validate, TYPICAL_TEST_DATA)
+        st = stats.load(filename)
+        st.strip_dirs()
+        st.sort_stats('time', 'calls')
+        st.print_stats(20)
+        self.assertTrue(result)
+        self.fail()
