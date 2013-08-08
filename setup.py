a07c8b5dc14e16cbbfaa66bf20caaa174a4bcc8f
Eric Casteleijn <ec@trapit.com>
2013-08-07 19:00:31 -0700

    changed to top level module, added setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+import val
+
+setup(
+    name='val',
+    version=val.__version__,
+    author='Eric Casteleijn',
+    author_email='thisfred@gmail.com',
+    description='Python object validator',
+    license='BSD',
+    keywords='validation validators',
+    url='http://github.com/thisfred/val',
+    py_modules=['val'],
+    long_description=open('README.md').read())
--- val.py
+++ val.py
@@ -1,3 +1,6 @@
+__version__ = '0.1'
+
+
 class NotValid(Exception):
     pass
 
