60ceae6dbf72b08fc2607371d20a5d663f6f0a7d
Eric Casteleijn <ec@trapit.com>
2013-10-31 12:15:50 -0700

    add missing __str__ on NotValid

--- val.py
+++ val.py
@@ -13,6 +13,9 @@ class NotValid(Exception):
     def __init__(self, msg):
         self.msg = msg
 
+    def __str__(self):
+        return self.msg
+
 
 def parse_schema(schema):
     if isinstance(schema, Schema):
