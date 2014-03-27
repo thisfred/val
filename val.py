d3b12aa7376716462798dbc9e31b122e50d2b404
Eric Casteleijn <ec@trapit.com>
2014-03-26 19:05:42 -0700

    refactoring

--- val.py
+++ val.py
@@ -1,4 +1,6 @@
 """
+val: A validator for arbitrary python objects.
+
 Copyright (c) 2013
 Eric Casteleijn, <thisfred@gmail.com>
 """
@@ -9,142 +11,220 @@ NOT_SUPPLIED = object()
 
 
 class NotValid(Exception):
+    """Object not valid for schema."""
+
     pass
 
 
 def get_repr(thing):
+    """Get sensible string representation for validator."""
     return (
         getattr(thing, 'func_doc') or
         getattr(thing, 'func_name') or
         repr(thing))
 
 
-def parse_schema(schema):
-    if isinstance(schema, Schema):
-        return schema.validate
+def build_type_validator(value_type):
+    """Build a validator that only checks the type of a value."""
 
-    if type(schema) is type:
-
-        def type_validator(data):
-            if isinstance(data, schema):
-                return data
-
-            raise NotValid('%r is not of type %r' % (data, schema))
-
-        return type_validator
-
-    if isinstance(schema, dict):
-
-        optional = {}
-        defaults = {}
-        mandatory = {}
-        types = {}
-        for key, value in schema.items():
-            if isinstance(key, Optional):
-                optional[key.value] = parse_schema(value)
-                if key.default is not NOT_SUPPLIED:
-                    defaults[key.value] = (key.default, key.null_values)
-                continue
-
-            if type(key) is type:
-                types[key] = parse_schema(value)
-                continue
-
-            mandatory[key] = parse_schema(value)
-
-        def dict_validator(data):
-            missing = defaults.keys()
-            if not isinstance(data, dict):
-                raise NotValid('%r is not of type dict' % (data,))
-            validated = {}
-            to_validate = data.keys()
-            for key, sub_schema in mandatory.items():
-                if key not in data:
-                    raise NotValid('missing key: %r' % (key,))
-                try:
-                    validated[key] = sub_schema(data[key])
-                except NotValid, e:
-                    raise NotValid('%s: %s' % (key, e.args))
-                to_validate.remove(key)
-            for key in to_validate:
-                value = data[key]
-                if key in optional:
-                    try:
-                        validated[key] = optional[key](value)
-                    except NotValid, e:
-                        raise NotValid('%s: %s' % (key, e.args))
-                    if key in missing:
-                        _, null_values = defaults[key]
-                        if null_values is not NOT_SUPPLIED:
-                            if validated[key] in null_values:
-                                continue
-                        missing.remove(key)
-                    continue  # pragma: nocover
-                for key_schema, value_schema in types.items():
-                    if not isinstance(key, key_schema):
-                        continue
-                    try:
-                        validated[key] = value_schema(value)
-                    except NotValid:
-                        continue
-                    else:
-                        break
-                else:
-                    raise NotValid('key %r and value %s not matched' % (
-                        key, value))
-            for key in missing:
-                validated[key] = defaults[key][0]
-            return validated
-
-        return dict_validator
-
-    if type(schema) in (list, tuple, set):
-
-        sub_schemas = [parse_schema(s) for s in schema]
-
-        def item_validator(value):
-            for sub in sub_schemas:
-                try:
-                    return sub(value)
-
-                except NotValid:
-                    pass
-
-            raise NotValid('%r not validated by anything in %s.' % (
-                value, schema))
-
-        def collection_validator(data):
-            if not type(data) is type(schema):
-                raise NotValid('%r is not of type %s' % (data, type(schema)))
-
-            return type(schema)(item_validator(value) for value in data)
-
-        return collection_validator
-
-    if callable(schema):
-
-        def callable_validator(data):
-            try:
-                if schema(data):
-                    return data
-                else:
-                    raise NotValid(
-                        "%r not validated by '%s'" % (data, get_repr(schema)))
-            except (TypeError, ValueError), e:
-                raise NotValid(*e.args)
-
-        return callable_validator
-
-    def static_validator(data):
-        if data == schema:
+    def type_validator(data):
+        """Validate instances of a particular type."""
+        if isinstance(data, value_type):
             return data
 
-        raise NotValid('%r is not equal to %r' % (data, schema))
+        raise NotValid('%r is not of type %r' % (data, value_type))
+
+    return type_validator
+
+
+def build_static_validator(exact_value):
+    """Build a validator that checks if the data is equal to an exact value."""
+
+    def static_validator(data):
+        """Validate by equality."""
+        if data == exact_value:
+            return data
+
+        raise NotValid('%r is not equal to %r' % (data, exact_value))
 
     return static_validator
 
 
-class Schema(object):
+def build_callable_validator(function):
+    """Build a validator that checks the return value of function(data)."""
+
+    def callable_validator(data):
+        """Validate by checking the return value of function(data)."""
+        try:
+            if function(data):
+                return data
+
+        except (TypeError, ValueError, NotValid), e:
+            raise NotValid(*e.args)
+
+        raise NotValid("%r not validated by '%s'" % (data, get_repr(function)))
+
+    return callable_validator
+
+
+def build_iterable_validator(iterable):
+    """Build a validator from an iterable."""
+    sub_schemas = [parse_schema(s) for s in iterable]
+
+    def item_validator(value):
+        """Validate items in an iterable."""
+        for sub in sub_schemas:
+            try:
+                return sub(value)
+
+            except NotValid:
+                pass
+
+        raise NotValid('%r not validated by anything in %s.' % (
+            value, iterable))
+
+    def iterable_validator(data):
+        """Validate an iterable."""
+        if not type(data) is type(iterable):
+            raise NotValid('%r is not of type %s' % (data, type(iterable)))
+
+        return type(iterable)(item_validator(value) for value in data)
+
+    return iterable_validator
+
+
+def _determine_keys(dictionary):
+    """Determine the different kinds of keys."""
+    optional = {}
+    defaults = {}
+    mandatory = {}
+    types = {}
+    for key, value in dictionary.items():
+        if isinstance(key, Optional):
+            optional[key.value] = parse_schema(value)
+            if key.default is not NOT_SUPPLIED:
+                defaults[key.value] = (key.default, key.null_values)
+            continue
+
+        if type(key) is type:
+            types[key] = parse_schema(value)
+            continue
+
+        mandatory[key] = parse_schema(value)
+    return mandatory, optional, types, defaults
+
+
+def _validate_mandatory_keys(mandatory, validated, data, to_validate):
+    """Validate the manditory keys."""
+    for key, sub_schema in mandatory.items():
+        if key not in data:
+            raise NotValid('missing key: %r' % (key,))
+        try:
+            validated[key] = sub_schema(data[key])
+        except NotValid, e:
+            raise NotValid('%s: %s' % (key, e.args))
+        to_validate.remove(key)
+
+
+def _validate_optional_key(key, missing, value, defaults, validated, optional):
+    """Validate an optional key."""
+    try:
+        validated[key] = optional[key](value)
+    except NotValid, e:
+        raise NotValid('%s: %s' % (key, e.args))
+    if key in missing:
+        _, null_values = defaults[key]
+        if null_values is not NOT_SUPPLIED:
+            if validated[key] in null_values:
+                return
+        missing.remove(key)
+
+
+def _validate_type_key(key, value, types, validated):
+    """Validate a key's value by type."""
+    for key_schema, value_schema in types.items():
+        if not isinstance(key, key_schema):
+            continue
+        try:
+            validated[key] = value_schema(value)
+        except NotValid:
+            continue
+        else:
+            break
+    else:
+        raise NotValid('key %r and value %s not matched' % (key, value))
+
+
+def _validate_other_keys(optional, types, missing, defaults, validated, data,
+                         to_validate):
+    """Validate the rest of the keys present in the data."""
+    for key in to_validate:
+        value = data[key]
+        if key in optional:
+            _validate_optional_key(
+                key, missing, value, defaults, validated, optional)
+            continue
+        _validate_type_key(key, value, types, validated)
+
+
+def build_dict_validator(dictionary):
+    """Build a validator from a dictionary."""
+    mandatory, optional, types, defaults = _determine_keys(dictionary)
+
+    def dict_validator(data):
+        """Validate dictionaries."""
+        missing = defaults.keys()
+        if not isinstance(data, dict):
+            raise NotValid('%r is not of type dict' % (data,))
+
+        validated = {}
+        to_validate = data.keys()
+        _validate_mandatory_keys(mandatory, validated, data, to_validate)
+        _validate_other_keys(
+            optional, types, missing, defaults, validated, data, to_validate)
+        for key in missing:
+            validated[key] = defaults[key][0]
+
+        return validated
+
+    return dict_validator
+
+
+def parse_schema(schema):
+    """Parse a val schema definition."""
+
+    if isinstance(schema, BaseSchema):
+        return schema.validate
+
+    if type(schema) is type:
+        return build_type_validator(schema)
+
+    if isinstance(schema, dict):
+        return build_dict_validator(schema)
+
+    if type(schema) in (list, tuple, set):
+        return build_iterable_validator(schema)
+
+    if callable(schema):
+        return build_callable_validator(schema)
+
+    return build_static_validator(schema)
+
+
+class BaseSchema(object):
+    """Base class for all Schema objects."""
+
+    def validates(self, data):
+        """Return True if schema validates data, False otherwise."""
+        try:
+            self.validate(data)
+            return True
+        except NotValid:
+            return False
+
+
+class Schema(BaseSchema):
+    """A val schema."""
 
     def __init__(self, schema, additional_validators=None):
         self._orig_schema = schema
@@ -155,6 +235,7 @@ class Schema(object):
         return repr(self._orig_schema)
 
     def validate(self, data):
+        """Return validated data, or raise NotValid."""
         validated = self.schema(data)
         for validator in self.additional_validators:
             if not validator(validated):
@@ -163,15 +244,9 @@ class Schema(object):
                         validated, get_repr(validator)))
         return validated
 
-    def validates(self, data):
-        try:
-            self.validate(data)
-            return True
-        except NotValid:
-            return False
-
 
 class Optional(object):
+    """Optional key in a dictionary."""
 
     def __init__(self, value, default=NOT_SUPPLIED, null_values=NOT_SUPPLIED):
         self.value = value
@@ -182,13 +257,15 @@ class Optional(object):
         return "<Optional: %r>" % (self.value,)
 
 
-class Or(Schema):
+class Or(BaseSchema):
+    """Validates if any of the subschemas do."""
 
     def __init__(self, *values):
         self.values = values
         self.schemas = tuple(parse_schema(s) for s in values)
 
     def validate(self, data):
+        """Validate data if any subschema validates it."""
         errors = []
         for sub in self.schemas:
             try:
@@ -201,13 +278,15 @@ class Or(Schema):
         return "<%s>" % (" or ".join(["%r" % (v,) for v in self.values]),)
 
 
-class And(Schema):
+class And(BaseSchema):
+    """Validates if all of the subschemas do."""
 
     def __init__(self, *values):
         self.values = values
         self.schemas = tuple(parse_schema(s) for s in values)
 
     def validate(self, data):
+        """Validate data if all subschemas validate it."""
         for sub in self.schemas:
             data = sub(data)
         return data
@@ -216,12 +295,14 @@ class And(Schema):
         return "<%s>" % (" and ".join(["%r" % (v,) for v in self.values]),)
 
 
-class Convert(Schema):
+class Convert(BaseSchema):
+    """Convert a value."""
 
     def __init__(self, conversion):
         self.convert = conversion
 
     def validate(self, data):
+        """Convert data or die trying."""
         try:
             return self.convert(data)
         except (TypeError, ValueError), e:
@@ -231,7 +312,8 @@ class Convert(Schema):
         return '<Convert: %r>' % (self.convert,)
 
 
-class Ordered(Schema):
+class Ordered(BaseSchema):
+    """Validate an ordered iterable."""
 
     def __init__(self, schemas):
         self._orig_schema = schemas
@@ -239,6 +321,7 @@ class Ordered(Schema):
         self.length = len(self.schemas)
 
     def validate(self, values):
+        """Validate if the values are validated one by one in order."""
         if self.length != len(values):
             raise NotValid(
                 "Does not have exactly %d values. (Got %d.)" % (
